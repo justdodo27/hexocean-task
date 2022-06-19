7bc068cb7a9945d68140c95a7662c0ef225cfb8e
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-06-19 10:38:24 +0200

    Heroku deployment db

--- image_uploader_project/settings.py
+++ image_uploader_project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.environ.get('DATABASE_URL'),
         'PORT': 5432,
     }
 }
