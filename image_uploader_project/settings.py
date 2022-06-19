dfd9f3e2357dc3ac345b0cc85a1700c8b5843a96
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-06-19 12:06:47 +0200

    Fix mediafiles

--- image_uploader_project/settings.py
+++ image_uploader_project/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # URL used to access the media
 MEDIA_URL = '/media/'
