2ce1a414af595c5c1557485593b64876d0ec1453
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-06-19 11:10:03 +0200

    Add static root

--- image_uploader_project/settings.py
+++ image_uploader_project/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
